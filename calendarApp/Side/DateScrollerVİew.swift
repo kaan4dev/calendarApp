5598d1ca58210bea5bd56dfe329cce6ce13182a2
kaancpapi <kaanc0754@gmail.com>
2023-09-28 23:28:18 +0300

    Initial commit

---
+++ calendarApp/Side/DateScrollerVİew.swift
@@ -0,0 +1,8 @@
+//
+//  DateScrollerVİew.swift
+//  calendarApp
+//
+//  Created by Kaan Çakır on 28.09.2023.
+//
+
+import Foundation
---
+++ calendarApp/Side/MonthStruct.swift
@@ -0,0 +1,20 @@
+import Foundation
+import SwiftUI
+
+
+struct MonthStruct
+{
+    var monthType: MonthType
+    var dayInt : Int
+    func day() -> String
+    {
+        return String(dayInt)
+    }
+}
+
+enum MonthType
+{
+    case Previous
+    case Current
+    case Next
+}
